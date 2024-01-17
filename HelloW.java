315f4db1c021519e28490d9ddfad49c0482cc8fa
Andres Tapiero <43012936+AndresTapiero@users.noreply.github.com>
2024-01-17 18:01:48 -0500

    Update HelloW.java

--- HelloW.java
+++ HelloW.java
@@ -3,6 +3,6 @@ public class HelloW {
     
     public static void main(String[] args) {
         //String name = os.getenv("USERNAME");
-        System.out.println("Hola desde script " + name);
+        System.out.println("Hola desde script ");
     }
 }
